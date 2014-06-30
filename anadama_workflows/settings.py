fabc6518d6dbac572f7ed3c4965c9225e2659f16
Randall Schwager <schwager@hsph.harvard.edu>
2014-06-30 20:50:00 +0000

    that looks prettier

--- anadama_workflows/settings.py
+++ anadama_workflows/settings.py
@@ -1,13 +1,13 @@
-class workflows(object):
+class workflows:
     product_directory = "mibc_products"
 
-    class metaphlan2(object):
+    class metaphlan2:
         bowtie2db = "/vagrant/metaphlandb/mpa.200.ffn"
         mpa_pkl   = "/vagrant/metaphlandb/mpa.200.pkl"
-    class sixteen(object):
+    class sixteen:
         otu_taxonomy = "/vagrant/data/ccfa/gg_13_8_otus/taxonomy/99_otu_taxonomy.txt"
         otu_refseq   = "/vagrant/data/ccfa/gg_13_8_otus/rep_set/99_otus.fasta"
-    class alignment(object):
+    class alignment:
         kegg_bowtie2_db = "/vagrant/data/ccfa/KEGG_reduced_bowtie2/KEGG_1"
 
 
