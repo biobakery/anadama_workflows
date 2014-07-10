8785f8d1c876bbac7c8edfe085da9d4c46cf0bc4
Randall Schwager <schwager@hsph.harvard.edu>
2014-07-10 14:54:22 +0000

    greengenes 13_5 should be the default

--- anadama_workflows/settings.py
+++ anadama_workflows/settings.py
@@ -5,8 +5,8 @@ class workflows:
         bowtie2db = "/vagrant/metaphlandb/mpa.200.ffn"
         mpa_pkl   = "/vagrant/metaphlandb/mpa.200.pkl"
     class sixteen:
-        otu_taxonomy = "/vagrant/data/ccfa/gg_13_8_otus/taxonomy/99_otu_taxonomy.txt"
-        otu_refseq   = "/vagrant/data/ccfa/gg_13_8_otus/rep_set/99_otus.fasta"
+        otu_taxonomy = "/vagrant/data/ccfa/gg_13_5_otus/taxonomy/99_otu_taxonomy.txt"
+        otu_refseq   = "/vagrant/data/ccfa/gg_13_5_otus/rep_set/99_otus.fasta"
     class alignment:
         kegg_bowtie2_db = "/vagrant/data/ccfa/KEGG_reduced_bowtie2/KEGG_1"
 
