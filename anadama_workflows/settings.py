88875b501003113fe9f953235b1306b4260911a8
Human Microbiome Project phase 2 portal <ibdmdb@ibdmdb-int2.broadinstitute.org>
2014-09-13 18:57:10 -0400

    Added noop to empty biom files.

--- anadama_workflows/settings.py
+++ anadama_workflows/settings.py
@@ -1,5 +1,6 @@
 class workflows:
     product_directory = "mibc_products"
+    verbose = ""
     class metaphlan2(object):
         bowtie2db = "/seq/ibdmdb/centos6/var/lib/bowtie2db/mpa.200.ffn"
         mpa_pkl   = "/seq/ibdmdb/centos6/var/lib/bowtie2db/mpa.200.pkl"
@@ -7,5 +8,5 @@ class workflows:
         otu_taxonomy = "/seq/ibdmdb/centos6/var/lib/gg_13_5_otus/taxonomy/97_otu_taxonomy.txt"
         otu_refseq   = "/seq/ibdmdb/centos6/var/lib/gg_13_5_otus/rep_set/97_otus.fasta"
     class alignment:
-        kegg_bowtie2_db = "/vagrant/data/ccfa/KEGG_reduced_bowtie2/KEGG_1"
+        kegg_bowtie2_db = "/seq/ibdmdb/centos6/var/lib/ccfa/KEGG_reduced_bowtie2/KEGG_1"
 
