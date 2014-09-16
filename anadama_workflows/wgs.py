977163645657ba36de34992b122edf40d34d43be
Randall Schwager <schwager@hsph.harvard.edu>
2014-09-16 01:33:46 +0000

    feature: make metaphlan2 workflow more general

--- anadama_workflows/wgs.py
+++ anadama_workflows/wgs.py
@@ -74,22 +74,22 @@ def metaphlan2(files_list, **opts):
     outfile       = new_file(addext(files_list[0], "metaphlan2"))
     bowtie2out    = new_file(addext(files_list[0], "bowtie2out.txt"))
 
-    all_opts = { 'bt2_ps'   : 'very-sensitive',
-                 'bowtie2db': settings.workflows.metaphlan2.bowtie2db,
-                 'mpa_pkl'  : settings.workflows.metaphlan2.mpa_pkl,
-    }
-    all_opts.update(opts)
-    all_opts = dict_to_cmd_opts(all_opts)
-
     seqtype = guess_seq_filetype(infiles_list[0])
     if seqtype not in ('fasta', 'fastq'):
         raise ValueError("Need sequences in fasta or fastq format")
 
+    all_opts = { 'bt2_ps'      : 'very-sensitive',
+                 'bowtie2db'   : settings.workflows.metaphlan2.bowtie2db,
+                 'mpa_pkl'     : settings.workflows.metaphlan2.mpa_pkl,
+                 "bowtie2out"  : bowtie2out,
+                 "output_file" : outfile,
+                 "input_type"  : biopython_to_metaphlan[seqtype]  }
+
+    all_opts.update(opts)
+    all_opts = dict_to_cmd_opts(all_opts)
+
     cmd = starters.cat(infiles_list, guess_from=infiles_list[0])
     cmd += (" | metaphlan2.py"
-            + " --bowtie2out="+bowtie2out
-            + " --output_file="+outfile
-            + " --input_type="+biopython_to_metaphlan[seqtype]
             + " "+all_opts )
 
     return dict(name     = "metaphlan2:"+outfile,
