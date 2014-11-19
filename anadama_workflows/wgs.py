22a7fae25e90148883ab6495a35375d0bb938d80
Randall Schwager <schwager@hsph.harvard.edu>
2014-11-19 20:54:47 +0000

    feature: add humann2 workflow

--- anadama_workflows/wgs.py
+++ anadama_workflows/wgs.py
@@ -55,6 +55,43 @@ def humann(infiles_list, workdir):
         ]
     }
         
+
+@requires(binaries=['humann2.py'],
+          version_methods=['pip freeze | grep humann2lib'])
+def humann2(seqfile_in, output_dir, **opts):
+
+    default_opts = {
+        "input"  : seqfile_in,
+        "output" : os.path.realpath(output_dir),
+        
+        "metaphlan_bowtie2db": settings.workflows.metaphlan2.bowtie2db,
+        "metaphlan_pkl"      : settings.workflows.metaphlan2.mpa_pkl,
+        "uniref"             : settings.workflows.humann2.uniref_path,
+        "chocophlan"         : settings.workflows.humann2.chocophlan_path,
+        "pathways_databases" : settings.workflows.humann2.pathways_databases,
+        "o_log"              : os.path.join(output_dir, "humann2_log.txt"),
+        "output_format"      : "tsv"
+    }
+    default_opts.update(opts)
+    opts_str = dict_to_cmd_opts(default_opts, sep=" ")
+
+    cmd = "humann2.py " + opts_str
+
+    suffix = default_opts['output_format']
+    _join = lambda s: new_file(os.path.basename(seqfile_in) + s,
+                               basedir=default_opts['output'] )
+    targets = map(_join, ("_genefamilies."+suffix, 
+                          "_pathcoverage."+suffix, 
+                          "_pathabundance."+suffix))
+
+    return {
+        "name"     : "humann2:"+output_dir,
+        "file_dep" : [seqfile_in],
+        "targets"  : targets,
+        "actions"  : [cmd]
+    }
+
+
 @requires(binaries=['metaphlan2.py'], 
           version_methods=['metaphlan2.py --version'])
 def metaphlan2(files_list, **opts):
