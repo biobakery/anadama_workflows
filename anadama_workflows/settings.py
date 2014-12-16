17b8e1e497076e8e4a751e3947b66dbfb8ff17a9
Keith Bayer <kbayer@hutlab11.rc.fas.harvard.edu>
2014-12-16 10:51:15 -0500

    Updated local paths to databases.

--- anadama_workflows/settings.py
+++ anadama_workflows/settings.py
@@ -6,13 +6,13 @@ class workflows:
         bowtie2db = "/net/hutlab11/srv/export/hutlab11/share_root/databases/metaphlan2/db_v20/mpa_v20_m200"
         mpa_pkl   = "/net/hutlab11/srv/export/hutlab11/share_root/databases/metaphlan2/db_v20/mpa_v20_m200.pkl"
     class sixteen:
-        otu_taxonomy = "/net/hutlab11/srv/export/hutlab11/share_root/databases/ccfa/gg_13_5_otus/taxonomy/97_otu_taxonomy.txt"
-        otu_refseq   = "/net/hutlab11/srv/export/hutlab11/share_root/databases/ccfa/gg_13_5_otus/rep_set/97_otus.fasta"
+        otu_taxonomy = "/net/hutlab11/srv/export/hutlab11/share_root/databases/gg_13_5_otus/taxonomy/97_otu_taxonomy.txt"
+        otu_refseq   = "/net/hutlab11/srv/export/hutlab11/share_root/databases/gg_13_5_otus/rep_set/97_otus.fasta"
     class alignment:
         kegg_bowtie2_db = "/net/hutlab11/srv/export/hutlab11/share_root/databases/ccfa/KEGG_reduced_bowtie2/KEGG_1"
                                              
     class humann2:
-        install_dir = "/net/hutlab11/srv/export/hutlab11/share_root/databases/humann2/databases"
+        install_dir = "/net/hutlab11/srv/export/hutlab11/share_root/databases/humann2"
         uniref_path = install_dir+"/uniref/"
         chocophlan_path = install_dir+"/chocophlan/"
         _pathway_dir = install_dir+"/pathways"
