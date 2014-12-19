5630c2ec58641829284d8bd0c3dc4848303cff8b
Randall Schwager <schwager@hsph.harvard.edu>
2014-12-19 20:58:49 +0000

    feature: inner join on sequence pair, lower memory usage and quicker runtime for sorted sequences

--- anadama_workflows/utility_scripts/pair.py
+++ anadama_workflows/utility_scripts/pair.py
@@ -4,10 +4,12 @@ import re
 import logging
 import optparse
 from pprint import pformat
-from itertools import izip_longest, ifilter
+from itertools import izip_longest, ifilter, count, izip
 
 from Bio import SeqIO
 from Bio import BiopythonParserWarning
+
+DEBUG = False
 
 HELP="""%prog [options] -f <format> <read_1_file> <read_2_file>
 
@@ -45,6 +47,15 @@ opts_list = [
                          dest="logging", default="INFO",
                          help="Logging verbosity, options are debug, info, "+
                          "warning, and critical"),
+    optparse.make_option('-i', '--inner_join', action="store", 
+                         dest="inner_join", default=None,
+                         help="Assume the sequences are sorted and perform a "
+                         "left or right inner join of one sequence set to the "
+                         "other. Acceptable values are 'left' and 'right'. "
+                         "Defaults to an equi-join, where any sequence on "
+                         "either side is dropped if no pair is found. Using "
+                         "either 'left' or 'right' reduces runtime and memory "
+                         "resources"),
     optparse.make_option('-m', '--no_mangle', action="store_false", 
                          default=True, dest="mangle",
                          help="Output sequence labels with just the ID in "
@@ -92,23 +103,7 @@ def _pair_reads(keys, reads):
             read_cache2[k2] = r2
 
 
-def extract_compare_key(reads, regex):
-    for read in reads:
-        if not read:
-            yield None
-            continue
-        match = regex.match(read.id)
-        if not match or not match.groups():
-            raise ValueError("Provided regex has no matches"
-                             " or gives no groups against string "+read.id)
-        else:
-            yield match.group(1)
-        
-
-def pair_reads(in1, in2, id_match_pattern, parse_format):
-    regex = re.compile(id_match_pattern)
-    seqs = izip_longest(SeqIO.parse(in1, parse_format),
-                        SeqIO.parse(in2, parse_format))
+def _pair_reads_cached(seqs, regex):
     for i, reads in enumerate(seqs):
         try:
             keys = extract_compare_key(reads, regex)
@@ -124,12 +119,70 @@ def pair_reads(in1, in2, id_match_pattern, parse_format):
             logging.exception(e)
             continue
 
-
-    if logging.getLogger().isEnabledFor(logging.DEBUG):
+    if DEBUG:
         global read_cache1, read_cache2
         logging.debug("Dropped %i orphaned reads:", 
                       len(read_cache1)+len(read_cache2))
         logging.debug(read_cache1.keys()+read_cache2.keys())
+
+
+def _pair_reads_sorted(join_direction="left"):
+    """Assume the sequences come pre-sorted and perform a right or left
+    inner join, depending on the ``join_direction`` keyword argument."""
+
+    def _join(forward_reads, reverse_reads, cmp_regex):
+        counter = count(1)
+        r1s, r2s = forward_reads, reverse_reads
+        if join_direction.lower() == "left":
+            def _discard(r1, r2, k1, k2):
+                logging.debug("Dropped read: %s", k2)
+                i, r2 = counter.next(), r2s.next()
+                k2 = extract_compare_key([r2], cmp_regex, track=i).next()
+                return i, r1, r2, k1, k2
+
+        else:
+            def _discard(r1, r2, k1, k2):
+                logging.debug("Dropped read: %s", k1)
+                i, r1 = counter.next(), r1s.next()
+                k1 = extract_compare_key([r1], cmp_regex, track=i).next()
+                return i, r1, r2, k1, k2
+
+        while True:
+            i, r1, r2 = counter.next(), r1s.next(), r2s.next()
+            k1, k2 = extract_compare_key((r1, r2), cmp_regex, track=i)
+            while k1 != k2:
+                i, r1, r2, k1, k2 = _discard(r1, r2, k1, k2)
+            yield r1, r2
+
+
+    return _join
+
+
+def extract_compare_key(reads, regex, track=0):
+    for read in reads:
+        if not read:
+            yield None
+            continue
+        match = regex.match(read.id)
+        if not match or not match.groups():
+            logging.warning("Provided regex has no matches or gives no "
+                            "groups against string %s on line %i", 
+                            read.id, track)
+        else:
+            yield match.group(1)
+        
+
+def pair_reads(in1, in2, id_match_pattern, parse_format, join_direction=None):
+    regex = re.compile(id_match_pattern)
+    if join_direction:
+        pairer = _pair_reads_sorted(join_direction=join_direction)
+        return pairer(forward_reads = SeqIO.parse(in1, parse_format),
+                      reverse_reads = SeqIO.parse(in2, parse_format),
+                      cmp_regex     = regex)
+    else:
+        seqs = izip_longest(SeqIO.parse(in1, parse_format),
+                            SeqIO.parse(in2, parse_format))
+        return _pair_reads_cached(seqs, regex)
 
 
 def _output(read_pairs, output_pair, output_format, only_id=True):
@@ -140,7 +193,7 @@ def _output(read_pairs, output_pair, output_format, only_id=True):
             r2.description = r2.id + " 2"
         SeqIO.write(r1, out1, output_format)
         SeqIO.write(r2, out2, output_format)
-        if logging.getLogger().isEnabledFor(logging.DEBUG):
+        if DEBUG:
             if i % 100 == 0 and i != 0:
                 logging.debug("Wrote %d records", i)
 
@@ -148,7 +201,10 @@ def _output(read_pairs, output_pair, output_format, only_id=True):
 def main():
     opts, (r1_fname, r2_fname) = handle_cli()
 
-    logging.getLogger().setLevel(getattr(logging, opts.logging.upper()))
+    global DEBUG
+    level = getattr(logging, opts.logging.upper())
+    logging.getLogger().setLevel(level)
+    DEBUG = logging.getLogger().isEnabledFor(logging.DEBUG)
     logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s")
 
     r1out_fname, r2out_fname = generate_fnames(opts, r1_fname, r2_fname)
@@ -159,7 +215,8 @@ def main():
            open(r2_fname,    'r') as r2in:
         paired = pair_reads(in1=r1in, in2=r2in,
                             id_match_pattern=opts.compare_regex,
-                            parse_format=opts.to_format)
+                            parse_format=opts.to_format,
+                            join_direction=opts.inner_join)
         _output(paired, (r1out, r2out),
                 output_format=opts.to_format, only_id=opts.mangle)
 
