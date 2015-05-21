3221543a4c04c7935d0b32871e2c6ab86d53f9b8
schwager-hsph <schwager@hsph.harvard.edu>
2015-05-21 16:58:39 -0400

    just for reference

--- anadama_workflows/settings.py
+++ anadama_workflows/settings.py
@@ -1,20 +1,25 @@
+base = "/home/rschwager/anadama_dev/"
+
 class workflows:
     product_directory = "anadama_products"
     verbose           = True
 
     class metaphlan2:
-        bowtie2db = "/vagrant/scripts/metaphlan2/db_v20/mpa_v20_m200"
-        mpa_pkl   = "/vagrant/scripts/metaphlan2/db_v20/mpa_v20_m200.pkl"
+        bowtie2db = base+"bin/db_v20/mpa_v20_m200"
+        mpa_pkl   = base+"bin/db_v20/mpa_v20_m200.pkl"
     class sixteen:
-        otu_taxonomy = "/vagrant/data/ccfa/gg_13_5_otus/taxonomy/97_otu_taxonomy.txt"
-        otu_refseq   = "/vagrant/data/ccfa/gg_13_5_otus/rep_set/97_otus.fasta"
-    class alignment:
-        kegg_bowtie2_db = "/vagrant/data/ccfa/ccfa/KEGG_reduced_bowtie2/KEGG_1"
-                                             
+        otu_taxonomy = base+"databases/gg_13_5_otus/taxonomy/97_otu_taxonomy.txt"
+        otu_refseq   = base+"databases/gg_13_5_otus/rep_set/97_otus.fasta"
+    class picrust:
+        copy_number = base+"src/picrust-1.0.0/picrust/data/16S_13_5_precalculated.tab.gz"
     class knead:
-        reference_db = "/vagrant/scripts/kneaddata/databases/bowtie2/humanGRCh38"
-        trim_path = "/vagrant/scripts/Trimmomatic-0.33/trimmomatic-0.33.jar"
+        reference_db = base+"databases/bowtie2/humanGRCh38"
+        trim_path = base+"src/Trimmomatic-0.33/trimmomatic-0.33.jar"
 
     class subread:
         index = "/vagrant/data/subread/GRCh38"
         annotations="/vagrant/data/subread/default_annotations.gtf"
+
+    class usearch:
+        chimera_gold_standard = base+"databases/uchime/gold.fa"
+        otu_db = base+"databases/usearch/gg_13_5_97-otus.udb"
