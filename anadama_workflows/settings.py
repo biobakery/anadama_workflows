ec4922f36e8d21ad4fe028616f3a17aa3eeecd5c
Randall Schwager <schwager@hsph.harvard.edu>
2015-11-04 11:53:59 -0500

    add hutlab11 settings

--- anadama_workflows/settings.py
+++ anadama_workflows/settings.py
@@ -1,26 +1,24 @@
-base = "/home/rschwager/anadama_dev/"
+base = "/net/hutlab11/srv/export/hutlab11/share_root/"
 
 class workflows:
     product_directory = "anadama_products"
     verbose           = True
 
     class metaphlan2:
-        bowtie2db = base+"bin/db_v20/mpa_v20_m200"
-        mpa_pkl   = base+"bin/db_v20/mpa_v20_m200.pkl"
+        bowtie2db = base+"databases/metaphlan2/db_v20/mpa_v20_m200"
+        mpa_pkl   = base+"databases/metaphlan2/db_v20/mpa_v20_m200.pkl"
     class sixteen:
         otu_taxonomy = base+"databases/gg_13_5_otus/taxonomy/97_otu_taxonomy.txt"
         otu_refseq   = base+"databases/gg_13_5_otus/rep_set/97_otus.fasta"
-    class picrust:
-        copy_number = base+"src/picrust-1.0.0/picrust/data/16S_13_5_precalculated.tab.gz"
+    class alignment:
+        kegg_bowtie2_db = base+"databases/ccfa/KEGG_reduced_bowtie2/KEGG_1"
     class knead:
-        reference_db = [
-            base+"databases/bowtie2/human/humanGRCh38"
-        ]
-
+        reference_db = [ base+"databases/bowtie2/humanGRCh38"]
+    class picrust:
+        copy_number = base+"tools/anadama/picrust/tmp/picrust/picrust/data/16S_13_5_precalculated.tab.gz"
     class subread:
         index = "/vagrant/data/subread/GRCh38"
         annotations="/vagrant/data/subread/default_annotations.gtf"
-
     class usearch:
         chimera_gold_standard = base+"databases/uchime/gold.fa"
         otu_db = base+"databases/usearch/gg_13_5_97-otus.udb"
